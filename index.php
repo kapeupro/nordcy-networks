0221b65ff27986cb7c99eb5313a239b4a60a3306
dimitrisarrazinkapeu <dimitri.sarrazin.pro.dev@gmail.com>
2022-01-11 12:14:28 +0100

    :construction: Attention ne pas toucher au footer svp je modifier les petite erreur avec le css :construction_worker: :alien:

--- index.php
+++ index.php
@@ -7,15 +7,35 @@ include('inc/header.php');
         <img src="asset/img/globe.svg">
         <h1>Sécurisez votre réseau <br>
             grâce à NORDY NETWORK </h1>
-    </div>
-    <div id="cta">
-        <div class="cta-header-ins">
-            <a href="inscription.php">Inscription</a>
+        <div id="cta">
+            <div class="cta-header-ins">
+                <a href="inscription.php">Inscription</a>
+            </div>
+            <div class="cta-header-con">
+                <a href="connexion.php">Connexion</a>
+            </div>
         </div>
-        <div class="cta-header-con">
-            <a href="connexion.php.php">Inscription</a>
+        <div class="cta-fleche">
+            <!-- Ajout par la suite l'animations avec le boutton-->
+            <img src="asset/img/fleche.svg" alt="fleche">
         </div>
     </div>
+</section>
+
+<section id="container-a-pro">
+   <div class="container-pro">
+       <div class="item-pro-0">
+           <h2>A propos de <br>Nordcy Network</h2>
+
+       </div>
+       <div class="item-pro-1">
+           <p>Nordcy Network est une société innovante, spécialisée dans la sécurité des systèmes d’information. Ses experts sont mondialement reconnus pour leurs activités de R & D dans le domaine des systèmes de détection d’intrusion et de réaction contre les cyberattaques.
+               <br>
+               <br> Nordcy Network fournit des solutions pour la défense active contre les attaques sur les systèmes et réseaux d’information pour garantir la disponibilité et la qualité des services de ses clients.
+               <br>
+               <br> Ces solutions sont de nature à répondre aux problématiques des acteurs de l’internet que ce soit pour leurs propres besoins ou pour offrir un service à valeur ajoutée à leurs clients.</p>
+       </div>
+   </div>
 </section>
 
 
