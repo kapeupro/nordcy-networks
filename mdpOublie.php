90dabf9f975504b9a79bb06cd1bbc813883eb2ba
anaiscrenier <anais000@hotmail.fr>
2022-01-12 16:34:25 +0100

    parti mot de passe oublié faite avec une parti droit pour la connexion

--- mdpOublie.php
+++ mdpOublie.php
@@ -27,36 +27,44 @@ include ('inc/header.php');
             </div>
             <div class="form-container sign-in-container">
                 <form action="#" method="post">
-                    <h1>Se connecter</h1>
+                    <h1>Mot de pass oublié</h1>
                     <div>
                         <label for="email"></label>
-                        <input type="email" id="email" placeholder="Email">
+                        <input type="email" id="email" placeholder="Adresse mail">
                         <span class="error"></span>
                     </div>
                     <div>
                         <label for="password"></label>
-                        <input type="password" id="password" placeholder="Mot de passe">
+                        <input type="password" id="password" placeholder=" Nouveau Mot de passe">
+                        <span class="error"></span>
+                    </div>
+                    <div>
+                        <label for="password"></label>
+                        <input type="password" id="password" placeholder=" confirmation Mot de passe">
                         <span class="error"></span>
                     </div>
 
-                    <div class="mdp_oublié">
-                        <a href="mdpOublie.php"> Mot de passe oublié ?</a>
-                    </div>
-
-                    <button>Connexion</button>
+                    <button>Envoyer</button>
                 </form>
             </div>
             <div class="overlay-container">
                 <div class="overlay">
-                    <div class="overlay-panel overlay-left">
-                        <h1>Bon retour !</h1>
-                        <p>Si tu as déjà un compte, connecte-toi !</p>
-                        <button class="ghost" id="signIn">Connexion</button>
-                    </div>
                     <div class="overlay-panel overlay-right">
-                        <h1>Bienvenue</h1>
-                        <p>Si tu n'as pas de compte et que tu souhaites nous rejoindre</p>
-                        <button class="ghost" id="signUp"><a href="inscription.php">S'inscrire</a></button>
+                        <form action="#" method="post">
+                            <h1>Se connecter</h1>
+                            <div>
+                                <label for="email"></label>
+                                <input type="email" id="email" placeholder="Email">
+                                <span class="error"></span>
+                            </div>
+                            <div>
+                                <label for="password"></label>
+                                <input type="password" id="password" placeholder="Mot de passe">
+                                <span class="error"></span>
+                            </div>
+
+                            <button>Connexion</button>
+                        </form>
                     </div>
                 </div>
             </div>
