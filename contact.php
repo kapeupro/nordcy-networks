2d3f31716c3e115b7d59d52a44eaf8edf1aca25f
thibaultcoge <thibaultcoge@hotmail.fr>
2022-01-12 16:54:09 +0100

    page contact html&css fini

--- contact.php
+++ contact.php
@@ -3,24 +3,30 @@
 include ('inc/header.php'); ?>
 
 <section id="contact">
-    <div class="wrap">
-        <form action="#">
-            <p>Nous Contacter</p>
-              <div class="contact_input">
-                  <label for="prénom"></label>
-                  <input type="text" id="prenom" placeholder="Prénom" />
-                  <span class="error"></span>
-                  <label for="email"></label>
-                  <input type="email" id="email" placeholder="Email" />
-                  <span class="error"></span>
-                  <label for="message"></label>
-                  <input type="text" id="message" placeholder="Votre message">
-                  <span class="error"></span>
-              </div>
-            <div class="contact_button">
+    <div class="container" id="container">
+        <div class="form-container sign-in-container">
+            <form action="#">
+                    <h1>Nous Contacter</h1>
+                <label for="prenom"></label>
+                <input type="text" id="prenom" placeholder="Prénom" />
+                <span class="error"></span>
+                <label for="email"></label>
+                <input type="email" id="email" placeholder="Email" />
+                <span class="error"></span>
+                <label for="message"></label>
+                <input type="text" id="message" placeholder="Message" />
+                <span class="error"></span>
                 <button>Envoyer</button>
+            </form>
+        </div>
+        <div class="overlay-container">
+            <div class="overlay">
+                <h1>Coordonnées</h1>
+                <p>Numéro 01 70 48 90 43</p>
+                <p>Du Lundi au Vendredi de 9:00 à 18:00</p>
+                <p>Le Samedi de 10:00 à 18:00</p>
             </div>
-        </form>
+        </div>
     </div>
 </section>
 
