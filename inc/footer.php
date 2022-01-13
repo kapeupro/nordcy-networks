5693bf1bbe00f9eef33c9a06aecaa4fbf3c8e8ce
anaiscrenier <anais000@hotmail.fr>
2022-01-13 12:34:40 +0100

    recuperation du pull de dimitri pour tester en local le pdo

--- inc/footer.php
+++ inc/footer.php
@@ -2,7 +2,7 @@
 <footer id="footer">
     <div class="container-footer">
         <p>&copy; NORDCY NETWORK SAS 2010-2021 -30 Pl. Henri Gadeau de Kerville, 76100 Rouen</p>
-        <a href="#">Mention légales</a>
+        <a href="mentionslegal.php">Mentions légal</a>
     </div>
     <div class="logo">
        <ul>
