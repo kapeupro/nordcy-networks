3a00490c5afb2a80c597ac409143d43f4796cf06
thibaultcoge <thibaultcoge@hotmail.fr>
2022-01-14 10:46:52 +0100

    début dashboard&headerdash

---
+++ dashboard.php
@@ -0,0 +1,21 @@
+<?php
+include ('inc/headerdash.php') ?>
+
+
+<section id="dashboard">
+    <div class="wrap">
+        <div class="dashboard_container1">
+            <div class="dashboard_buttons">
+                <input type="submit" value="Dashboard">
+                <input type="submit" value="Logs">
+                <input type="submit" value="Détails">
+            </div>
+            <div class="dashboard_graph">
+
+            </div>
+        </div>
+        <div class="dashboard_container2">
+
+        </div>
+    </div>
+</section>
---
+++ inc/headerdash.php
@@ -0,0 +1,15 @@
+<section id="headerdash">
+    <div class="wrap">
+        <div class="header">
+            <div class="header_title">
+                <p>NORDCY NETWORK</p>
+            </div>
+            <div class="header_nav">
+                <a href="#">Accueil</a>
+                <a href="#">Dashboard</a>
+                <a href="#">Logs</a>
+                <a href="#">Détails</a>
+            </div>
+        </div>
+    </div>
+</section>
