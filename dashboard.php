3d80829b607ab19e4d684ce37b4310591fe6f2be
Dimitri Sarrazin <62432689+kapeupro@users.noreply.github.com>
2022-01-15 15:22:07 +0100

    test

--- dashboard.php
+++ dashboard.php
@@ -9,16 +9,22 @@ include ('inc/headerdash.php')
     <div class="wrap">
         <div class="dashboard_container1">
             <div class="dashboard_buttons">
-                <input type="submit" value="Dashboard">
-                <input type="submit" value="Logs">
-                <input type="submit" value="Détails">
+                <div class="dashboard_btn">
+                    <a href="#">Dashboard</a>
+                </div>
+                <div class="logs_btn">
+                    <a href="#">Logs</a>
+                </div>
+                <div class="details_btn">
+                     <a href="#">Détails</a>
+                </div>
             </div>
             <div class="dashboard_graph">
-
+                        <!--Graph 1-->
             </div>
         </div>
         <div class="dashboard_container2">
-
+                        <!--Graph 2-->
         </div>
     </div>
 </section>
