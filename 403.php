4d490693a24d1899482fa0f1e1e7f1978cafa88c
Dimitri Sarrazin <62432689+kapeupro@users.noreply.github.com>
2022-01-15 12:43:07 +0100

    ajout page 403 & 404 en html manque juste les page en css

---
+++ 403.php
@@ -0,0 +1,19 @@
+<?php
+
+
+include('inc/header.php');
+
+    <section id="error403">
+    <div class="container-403">
+        <div class="item-403">
+            <h2>403</h2>
+            <h3>Accès refusé</h3>
+            <p>Vous n'avez pas les droits d'accès à cette page</p>
+            <a href="index.php">Retour à l'accueil</a>
+        </div>
+    </div>
+    </section>
+
+
+include('inc/footer.php');
+?>
--- 404.php
+++ 404.php
@@ -1,3 +1,19 @@
 <?php
 
+
+include('inc/header.php');
+
+    <section id="error404">
+    <div class="container-404">
+        <div class="item-404">
+            <h2>404</h2>
+            <h3>Page introuvable</h3>
+            <p>La page que vous recherchez n'existe pas</p>
+            <a href="index.php">Retour à l'accueil</a>
+        </div>
+    </section>
+
+
+
+include('inc/footer.php');
 ?>
