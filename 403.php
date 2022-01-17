c452051124afdcfe93785e4f963f0e2283416758
anaiscrenier <anais000@hotmail.fr>
2022-01-17 10:37:49 +0100

    page 403-404 fini avec le style

---
+++ 403.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="asset/css/style.css">
+    <title>Accès interdit ! Vous ne pouvez pas accéder à cette page. - Nordcy-networks</title>
+</head>
+<body>
+
+<div id="notfound">
+    <div class="notfound">
+        <div class="notfound-403">
+            <h1>4<span>0</span>3</h1>
+        </div>
+        <h2>Vous devez être connecté pour l'accès a cette page.</h2>
+        <div class="notfound-connexion">
+            <div class="btn-error">
+                <a href="connexion.php"></a>
+                <button class="btn-404-403">Connexion</button>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+
+</html>
---
+++ donnees-personnelles-et-cookies.php
@@ -0,0 +1,92 @@
+<?php
+
+include('inc/header.php');
+
+?>
+
+
+
+    <div class="particookies">
+        <h1>Données personnelles et cookies</h1>
+    </div>
+
+    <div class="contenu-cookies">
+
+
+        <h2>Collecte de données</h2>
+        <br>
+
+        <p>Le présent site ne collecte pas de données personnelles.</p>
+        <br>
+
+        <h2>À propos des cookies</h2>
+        <br>
+
+        Lors de la consultation du site, plusieurs types de cookies sont déposés sur votre ordinateur,
+        votre mobile ou votre tablette. Un "cookie" est une suite d'informations, généralement de petite
+        taille et identifié par un nom, qui peut être transmis à votre navigateur par un site web sur lequel
+        vous vous connectez. Votre navigateur web le conservera pendant une certaine durée, et le renverra au serveur
+        web chaque fois que vous vous y re-connecterez.
+        <br><br>
+
+        <h2>Cookies techniques</h2>
+        <br>
+
+        Nous devons utiliser ces cookies pour pouvoir faire fonctionner certaines pages web. C’est la raison pour laquelle ils ne nécessitent pas votre consentement.
+        Nom : i18n
+        <br>
+        Services : Gestion du multilingue
+        <br>
+        Objectif : Redirige automatiquement vers la version du site dans la langue du navigateur de l'utilisateur.
+        <br>
+        Type de cookie et durée : cookie interne persistant, 1 an.
+        <br>
+        Nom : stonlyWidget
+        <br>
+        Services : Stonly
+        <br>
+        Objectif : Fournir un chatbot de FAQ pour fournir une aide supplémentaire à l'utilisateur.
+        <br>
+        Type de cookie et durée : cookie interne persistant.
+        <br><br>
+
+
+        <h2>Cookies de mesure d’audience</h2>
+        <br>
+
+        <p> Notre site utilise une solution de mesure d'audience qui ne dépose pas de cookies tiers.</p>
+        <br>
+
+        <h2>Quels sont vos droits sur vos données et comment les exercer ?</h2>
+        <br>
+
+        Vous disposez des droits suivants en ce qui concerne l’utilisation de vos données personnelles :
+        <br>
+
+        obtenir la confirmation que des données vous concernant sont ou ne sont pas traitées et, lorsqu’elles le sont,
+        l’accès à ces données ainsi
+        <br>
+        qu’à des informations sur ces traitements ;
+        <br>
+        obtenir la rectification de données inexactes ;
+        <br>
+        dans certains cas précis, obtenir l’effacement de certaines de vos données ;
+        <br>
+        dans certains cas précis, obtenir la limitation des traitements que nous réalisons ;
+        <br>
+        vous opposer au traitement de vos données, pour des raisons tenant à votre situation particulière, ou, indépendamment de votre situation
+        <br>
+        particulière, à l’utilisation de vos données à des fins de prospection ;
+        <br>
+        retirer pour l’avenir votre consentement à tout moment, s’il servait de base légale au traitement de vos données ;
+        <br>
+        recevoir les données que vous avez fournies et/ou demander de les transmettre à un autre responsable du traitement, si le traitement est
+        <br>
+        fondé sur votre consentement ou sur un contrat et que le traitement est automatisé ;
+        <br>
+        si vous résidez en France, définir le sort de vos données après votre mort.
+        <br>
+    </div>
+
+
+<?php include ('inc/footer.php');
